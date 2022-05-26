01c603d724328205b2a11afcb5c3ae1f219cbdd3
Evgeniy Makarov <makarov1994evgeniy@gmail.com>
2022-05-26 08:10:06 +0300

    feat: add section important, team and add css style

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,28 @@
-var slider = tns({
+const sliderMain = tns({
    container: '.slider',
    items: 1,
    slideBy: 'page',
    autoplay: false,
    controls: false,
-   navPosition: "bottom"
+   navPosition: "top"
 
- });
+});
+
+const slider = tns({
+   container: '.team__slider',
+   items: 1,
+   slideBy: 'page',
+   autoplay: false,
+   controls: false,
+   navPosition: "top"
+
+}); 
+
+
+document.querySelector('.prev').addEventListener('click', function () {
+   slider.goTo('prev');
+}); 
+
+document.querySelector('.next').addEventListener('click', function () {
+   slider.goTo('next');
+}); 
